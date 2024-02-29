89cbc59c58b98317ccc4de7a78989da41947b05c
Dele2009 <obedaminu303@gmail.com>
2024-02-29 14:33:38 +0100

    test-work

---
+++ src/Navbarcomponent/navbarportfolio.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import '../App.css'
+
+import { Link } from 'react-router-dom'
+
+import { NavHashLink } from 'react-router-hash-link'
+import { HiMenu } from 'react-icons/hi'
+import { IconContext } from 'react-icons'
+import { animateScroll } from 'react-scroll'
+
+function Mainportfolionavbar () {
+  const scrollup = () => {
+    animateScroll.scrollToTop()
+  }
+  return (
+    <>
+      <Navbar expand='lg' className='main-nav px-lg-5' fixed='top'>
+        <Container fluid>
+          <Navbar.Brand as={Link} to='/' className='fs-3'>
+            <code className='text-light'>
+              D.<span className='trans-back drop-shadow'>append(</span>aminu
+              <span className='trans-back drop-shadow'>)</span>
+            </code>
+          </Navbar.Brand>
+
+          <IconContext.Provider value={{ className: 'color-mix' }}>
+            <Navbar.Toggle
+              className='border-0 shadow-none'
+              aria-controls='navbarScroll'
+            >
+              <HiMenu />
+            </Navbar.Toggle>
+          </IconContext.Provider>
+
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='mt-4 mb-2 mt-lg-0 mb-lg-0 fs-4'
+              style={{ maxHeight: '300px' }}
+              navbarScroll
+            >
+              <Nav.Link
+                className='text-light'
+                as={NavHashLink}
+                to='/#hero-section'
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                className='text-light'
+                to='/#about-section'
+                as={NavHashLink}
+              >
+                About me
+              </Nav.Link>
+              <Nav.Link
+                className='text-light'
+                to='/#skills-section'
+                as={NavHashLink}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                className='text-light'
+                to='/#works-section'
+                as={NavHashLink}
+              >
+                Myworks
+              </Nav.Link>
+              <Nav.Link
+                className='text-light'
+                as={Link}
+                to='/projects'
+                onClick={() => scrollup()}
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link className='text-light' as={Link} to='/contact_Me'>
+                Contact Me
+              </Nav.Link>
+
+              {/* <Nav.Link
+                className='text-light'
+                href='https://products-page-phi.vercel.app'
+                target='_blank'
+              >
+                Projects
+              </Nav.Link> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+export default Mainportfolionavbar
