73c5dc9aa5ad0dcbcf994e7f27e4e68a4c6a2bb6
Dele2009 <obedaminu303@gmail.com>
2024-03-02 17:27:08 +0100

    skills update

--- src/Portfolio-Pages/skills.jsx
+++ src/Portfolio-Pages/skills.jsx
@@ -1,56 +1,63 @@
-import webInfo from "../webvalues.json";
+import webInfo from '../webvalues.json'
 
 const Skill = ({ skill, skillLogo, skillLength }) => {
-    return (
-        <div className="single-skill main-nav">
-            <p className="d-flex fs-5 justify-content-between align-items-center">
-                <img src={skillLogo} alt={`${skill}-logo`} id={`${skill}-logo`} width={60} height={60} /><span className="text-light">{skillLength}%</span>
-            </p>
-            <div className="bg-grey w-100">
-                <div className="skill2 bg-blue-grad"
-                    style={{
-                        width: skillLength + "%",
-                    }}></div>
+  return (
+    <div className='single-skill main-nav'>
+      <div className='d-flex fs-5 justify-content-between align-items-center'>
+        <img
+          src={skillLogo}
+          alt={`${skill}-logo`}
+          id={`${skill}-logo`}
+          width={60}
+          height={60}
+        />
+        <span className='text-light'>{skillLength}%</span>
+      </div>
+      <div className='bg-grey w-100 mt-1 '>
+        <div
+          className='skill2 bg-blue-grad'
+          style={{
+            width: skillLength + '%'
+          }}
+        ></div>
+      </div>
+    </div>
+  )
+}
+
+function Skills () {
+  const mySkills = webInfo.skillinfo
+  const newskills = mySkills.slice(0, 3)
+  const newskills2 = mySkills.slice(3, 6)
+  const newskills3 = mySkills.slice(6, 8)
+  return (
+    <>
+      <section
+        className='section-h d-flex justify-content-center align-items-center'
+        id='skills-section'
+      >
+        <div className='col-10'>
+          <p className='fs-1 text-center code-style pb-5'>Skills</p>
+          <div className='skill-con'>
+            <div className='d-flex justify-content-center align-items-center flex-lg-row flex-column gap-5 pb-5 '>
+              {newskills.map(newskill => {
+                return <Skill key={newskill.skill} {...newskill} />
+              })}
             </div>
-
+            <div className='d-flex justify-content-center align-items-center flex-lg-row flex-column gap-5 pb-5 '>
+              {newskills2.map(newskill2 => {
+                return <Skill key={newskill2.skill} {...newskill2} />
+              })}
+            </div>
+            <div className='d-flex justify-content-center align-items-center flex-lg-row flex-column gap-5 pb-5 '>
+              {newskills3.map(newskill3 => {
+                return <Skill key={newskill3.skill} {...newskill3} />
+              })}
+            </div>
+          </div>
         </div>
-    )
+      </section>
+    </>
+  )
 }
-
-function Skills() {
-    const mySkills = webInfo.skillinfo;
-    const newskills = mySkills.slice(0, 3);
-    const newskills2 = mySkills.slice(3, 6);
-    return (
-        <>
-            <section className="section-h d-flex justify-content-center align-items-center" id="skills-section">
-                <div className="col-10">
-
-                    <p className="fs-1 text-center code-style pb-5">Skills</p>
-                    <div className="skill-con">
-                        <div className="d-flex justify-content-center align-items-center flex-lg-row flex-column gap-5 pb-5 ">
-
-                            {
-                                newskills.map(newskill => {
-                                    return <Skill key={newskill.skill} {...newskill} />
-                                })
-                            }
-
-                        </div>
-                        <div className="d-flex justify-content-center align-items-center flex-lg-row flex-column gap-5 pb-5 ">
-                            {
-                                newskills2.map(newskill2 => {
-                                    return <Skill key={newskill2.skill} {...newskill2} />
-                                })
-                            }
-                        </div>
-                    </div>
-
-
-                </div>
-            </section>
-        </>
-
-    );
-}
-export default Skills;
+export default Skills
